933935663c72d5f57fc63ef4dea034476dffaa9b
Jake Toolson <jaketoolson@gmail.com>
2016-08-31 09:38:44 -0700

    turbo links is back

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ elixir(function (mix) {
         .scripts([
             'vendor/theme/scripts/jquery/jquery/dist/jquery.js',
             'vendor/vuejs/vue.1.0.26.js',
-            // 'vendor/turbolinks/turbolinks.5.0.0.js',
+            'vendor/turbolinks/turbolinks.5.0.0.js',
             'vendor/theme/scripts/jquery/tether/dist/js/tether.min.js',
             'vendor/theme/scripts/jquery/bootstrap/dist/js/bootstrap.js',
             'vendor/theme/scripts/jquery/underscore/underscore-min.js',
