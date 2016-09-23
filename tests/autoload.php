17ceb43c24fa25bb55238b53e28d5f3be4797d15
Jake Toolson <jaketoolson@gmail.com>
2016-09-22 18:55:16 -0700

    fix to strict check

--- tests/autoload.php
+++ tests/autoload.php
@@ -4,14 +4,14 @@ require __DIR__ .'/../bootstrap/autoload.php';
 $__app = require __DIR__.'/../bootstrap/app.php';
 $__app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
-if (env('APP_ENV') == 'testing') {
+if (env('APP_ENV') === 'testing') {
     $connection = new PDO("mysql:host=".env('DB_HOST')."", env('DB_USERNAME'), env('DB_PASSWORD'));
 
     $output = new Symfony\Component\Console\Output\ConsoleOutput();
     $output->writeln("<info>Truncating database...</info>");
 
-    $connection->query("DROP DATABASE IF EXISTS ".env('DB_DATABASE'))->execute();
-    $connection->query("CREATE DATABASE IF NOT EXISTS ".env('DB_DATABASE'))->execute();
+    $connection->query("DROP DATABASE IF EXISTS ".env('DB_TEST_DATABASE'))->execute();
+    $connection->query("CREATE DATABASE IF NOT EXISTS ".env('DB_TEST_DATABASE'))->execute();
     $output->writeln("<info>Migrating database...</info>");
     Artisan::call('migrate', ['--env' => 'testing']);
     $output->writeln("<info>Beginning tests</info>");
