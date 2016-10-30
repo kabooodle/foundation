2c14011a3d611567365840afc81562909256ee81
Jake Toolson <jaketoolson@gmail.com>
2016-10-29 22:37:15 -0700

    fix to web hook for stripe fix to clipboard js

--- app/Http/Controllers/Web/Webhooks/StripeWebhooksController.php
+++ app/Http/Controllers/Web/Webhooks/StripeWebhooksController.php
@@ -6,9 +6,10 @@
 
 namespace Kabooodle\Http\Controllers\Web\Webhooks;
 
-use Kabooodle\Bus\Events\User\UserSubscriptionCameOffTrial;
 use Kabooodle\Models\User;
+use Symfony\Component\HttpFoundation\Response;
 use Laravel\Cashier\Http\Controllers\WebhookController;
+use Kabooodle\Bus\Events\User\UserSubscriptionCameOffTrial;
 
 /**
  * Class StripeWebhooksController
@@ -16,14 +17,27 @@ use Laravel\Cashier\Http\Controllers\WebhookController;
  */
 class StripeWebhooksController extends WebhookController
 {
+    /**
+     * @param array $payload
+     *
+     * @return Response
+     */
     public function handleCustomerSubscriptionUpdated(array $payload)
     {
-        $user = User::where(($payload['data']['object']['customer']), 'stripe_id')->first();
-        if($user) {
-            if($payload['data']['previous_attributes']['status'] == "trialing" && $payload['data']['object']['status'] == "active") {
-                event(new UserSubscriptionCameOffTrial($user, $payload));
-                dd('fart');
-            }
+        /** @var User $user */
+        $user = $this->getUserByStripeId($payload['data']['object']['customer']);
+
+        // The major thing we care about is the fact that a user is no longer in trial
+        // and their account has rolled over to the first pay status.
+        // This can be confirmed be checking their previous status and current!
+        if($user
+            && $payload['data']['previous_attributes']['status'] == "trialing"
+            && $payload['data']['object']['status'] == "active") {
+
+            // We have an account that just got off trial, fire the event.
+            event(new UserSubscriptionCameOffTrial($user, $payload));
         }
+
+        return new Response('Webhook Handled', 200);
     }
 }
--- resources/assets/app/vendor.js
+++ resources/assets/app/vendor.js
@@ -1,26 +1,26 @@
-global.$ = global.jQuery = require ('jquery');
+global.$ = global.jQuery = require('jquery');
 global.Vue = require('vue/dist/vue.js');
-require ('vue-resource');
-// require ('vue-validator');
-global.Tether = require ('tether');
-require ('bootstrap');
-global._ = require ('underscore');
-require ('moment');
-require ('moment-timezone-tsc');
+require('vue-resource');
+// require('vue-validator');
+global.Tether = require('tether');
+require('bootstrap');
+global._ = require('underscore');
+require('moment');
+require('moment-timezone-tsc');
 
 // Use localized version of Noty because I've heavily modified it :)
 require('../../../resources/assets/vendor/jquery/noty/packaged/jquery.noty.packaged.js');
 
-require ('clipboard');
-require ('ekko-lightbox');
-require ('selectize');
-require ('emojione');
-require ('emojionearea');
-require ('bootstrap-touchspin');
-global.datetimepicker = require ('eonasdan-bootstrap-datetimepicker');
+global.Clipboard = require('clipboard');
+require('ekko-lightbox');
+require('selectize');
+require('emojione');
+require('emojionearea');
+require('bootstrap-touchspin');
+global.datetimepicker = require('eonasdan-bootstrap-datetimepicker');
 //
 // // Use the localized version because the NPM version is being fuckity.
-global.multiselect = require ('../vendor/bootstrap-multiselect/dist/js/bootstrap-multiselect');
+global.multiselect = require('../vendor/bootstrap-multiselect/dist/js/bootstrap-multiselect');
 
 // Vue.config.devtools = false;
 
