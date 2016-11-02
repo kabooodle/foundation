75f83ee60bb37205c7ccd9ecad71e8560789181c
Jake Toolson <jaketoolson@gmail.com>
2016-11-01 23:17:08 -0700

    working on abstract notification

---
+++ app/Bus/Events/Notificationables/AbstractNotificationable.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Kabooodle.
+ * Copyright (c) 2016. Jacob Toolson <jake@kabooodle.com>
+ */
+
+namespace Kabooodle\Events\Notificationables;
+
+/**
+ * Class AbstractNotificationable
+ * @package Kabooodle\Events\Notificationables
+ */
+abstract class AbstractNotificationable
+{
+    public function __construct()
+    {
+
+    }
+}
---
+++ app/Bus/Events/Notificationables/ReferralJoinedEvent.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This file is part of Kabooodle.
+ * Copyright (c) 2016. Jacob Toolson <jake@kabooodle.com>
+ */
+
+namespace Kabooodle\Events\Notificationables;
+
+use Kabooodle\Models\User;
+
+/**
+ * Class ReferralJoinedEvent
+ * @package Kabooodle\Events\Notificationables
+ */
+final class ReferralJoinedEvent extends AbstractNotificationable
+{
+    /**
+     * @var User
+     */
+    public $user;
+
+    /**
+     * @var User
+     */
+    public $referee;
+
+    /**
+     * ReferralJoinedEvent constructor.
+     *
+     * @param User $user
+     * @param User $referee
+     */
+    public function __construct(User $user, User $referee)
+    {
+        $this->user = $user;
+        $this->referee = $referee;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return User
+     */
+    public function getReferee(): User
+    {
+        return $this->referee;
+    }
+}
