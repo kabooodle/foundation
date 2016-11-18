9f549fc115dc6cf7b4b54b91a62e49f6ed4ad37a
Jake Toolson <jaketoolson@gmail.com>
2016-11-17 23:12:59 -0800

    random ass version :)

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.0.5';
+    const APP_VERSION = '0.1.0';
 
     /**
      * @return void
