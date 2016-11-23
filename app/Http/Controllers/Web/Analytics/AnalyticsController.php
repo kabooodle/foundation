2c520b2f7ff4bc1927d732ff52edb9416a225273
evals <evals@Matthew-Antrobuss-MacBook-Pro.local>
2016-11-23 13:08:35 -0800

    wip analytics

---
+++ app/Http/Controllers/Web/Analytics/AnalyticsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kabooodle\Http\Controllers\Web\Analytics;
+
+use Binput;
+use Illuminate\Http\Request;
+use Kabooodle\Http\Controllers\Web\Controller;
+
+/**
+ * Class AnalyticsController
+ */
+class AnalyticsController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function sales()
+    {
+
+    }
+
+    public function postings()
+    {
+
+    }
+}
