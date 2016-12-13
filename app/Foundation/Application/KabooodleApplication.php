5df12b0964c4cebb3030abff4fbdffa821531e46
Jake Toolson <jaketoolson@gmail.com>
2016-12-13 12:56:55 -0800

    facebook listing!

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.3.3';
+    const APP_VERSION = '0.4.0';
 
     /**
      * @return void
