3f43538bd6d64bdad488cc59b63d22ded7789f0b
Jake Toolson <jake@kabooodle.com>
2016-12-12 22:59:30 -0800

    iron for emails

--- app/Libraries/Emails/AbstractEmail.php
+++ app/Libraries/Emails/AbstractEmail.php
@@ -44,7 +44,7 @@ abstract class AbstractEmail
     /**
      * @var string
      */
-    protected $queueConnection;
+    protected $queueConnection = 'iron';
 
     /**
      * AbstractEmail constructor.
