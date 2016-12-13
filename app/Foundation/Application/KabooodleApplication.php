62ccb61a0a388f9f469d95f55d6eb797970724f6
Jake Toolson <jake@kabooodle.com>
2016-12-12 22:21:24 -0800

    fix to the cron route

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.3.0';
+    const APP_VERSION = '0.3.1';
 
     /**
      * @return void
