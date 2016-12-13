100c1e29e4a3c80d0bd5a70062b6c990d5f2fbd6
Jake Toolson <jaketoolson@gmail.com>
2016-12-12 18:47:26 -0800

    minor change to ellipsis

--- resources/views/listings/partials/_indexrow.blade.php
+++ resources/views/listings/partials/_indexrow.blade.php
@@ -15,7 +15,7 @@
     <td>
         <div class="pull-md-right">
             <div class="dropdown">
-                <a class="text-muted nav-link dropdown-toggle no-caret" href="#" data-toggle="dropdown">
+                <a class="text-muted btn btn-xs white dropdown-toggle no-caret" href="#" data-toggle="dropdown">
                     <i class="hidden-sm-down fa fa-ellipsis-h" aria-hidden="true"></i>
                     <span class="hidden-sm-up">Options</span>
                 </a>
