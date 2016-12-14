3ed104ee0b4adac14b76cdb2253b38d70786a508
Jake Toolson <jaketoolson@gmail.com>
2016-12-13 16:25:52 -0800

    this created a recursive bug

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.4.2';
+    const APP_VERSION = '0.4.3';
 
     /**
      * @return void
