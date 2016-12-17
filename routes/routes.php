4265b771b3e8c56f7242393a8df592e26f2036ed
Jake Toolson <jaketoolson@gmail.com>
2016-12-16 18:16:47 -0800

    order of operations for now.

--- routes/routes.php
+++ routes/routes.php
@@ -7,12 +7,13 @@ Route::get('privacy', function(){
     return view('content.privacy');
 });
 
-// routes registered to (any).domain.tld
-Route::group(['middleware' => ['web'], 'domain' => getEnvDomain(true)], function(){
-    require_once __DIR__ . DIRECTORY_SEPARATOR . 'web' . DIRECTORY_SEPARATOR . 'routes.php';
-});
-
 // Routes registered to api.kabooodle.tld
 Route::group(['domain' => 'api.'.getEnvDomain(true)], function($route){
     require_once __DIR__ . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 'routes.php';
+});
+
+
+// routes registered to (any).domain.tld
+Route::group(['middleware' => ['web']], function(){
+    require_once __DIR__ . DIRECTORY_SEPARATOR . 'web' . DIRECTORY_SEPARATOR . 'routes.php';
 });
