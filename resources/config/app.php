8132f04011fc9450e4c79a529a45354b7a789a87
Jake Toolson <jaketoolson@gmail.com>
2016-12-16 18:04:11 -0800

    Squashed commit of the following:
    commit 98a69c414ddf0e01b1d0559431198b006c053be8
    Author: Jake Toolson <jaketoolson@gmail.com>
    Date:   Fri Dec 16 17:59:30 2016 -0800
    
        still need to specify the domain
    
    commit 4ca32980943187a636780335514331501fa34c97
    Author: Jake Toolson <jaketoolson@gmail.com>
    Date:   Fri Dec 16 17:48:44 2016 -0800
    
        init
    
    commit 982d90d10e357bf986355ff39161726c7871f8e7
    Author: Jake Toolson <jaketoolson@gmail.com>
    Date:   Thu Dec 15 17:41:01 2016 -0800
    
        merged FB update

--- resources/config/app.php
+++ resources/config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://app.kabooodle.com'),
+    'url' => env('APP_URL', 'https://kabooodle.com'),
 
     /*
     |--------------------------------------------------------------------------
