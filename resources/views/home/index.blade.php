d2fa99adf8158f935d5ec73b169c28ce070e027f
Jake Toolson <jaketoolson@gmail.com>
2016-12-18 22:24:50 -0800

    working on more content

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,17 +1,52 @@
 @extends('layouts.home')
 
 @section('content')
+    <style>
+        #hero h1 {
+            font-size: 50px !important;
+            font-weight: 700 !important;
+        }
+        #hero h2 {
+            font-size: 20px !important;
+            line-height: 1.5rem;
+        }
+        #hero {
+            background: url('/assets/images/BG/a.png') top;
+            background-size: cover;
+        }
+    </style>
 
-    <div style="margin-top: 64px; height: 600px; padding-top: 60px" class="prpl-1000">
-        <div class="container">
+    <div style="margin-top: 64px; height: 600px; padding-top: 60px" class="prpl-1000" id="hero">
+        <div class="p-a">
             <div class="row">
-                <h1 class="text-white">A social network simplifying your inventory management,
-                    client communications, and more!
-                </h1>
+                <div class="col-sm-5">
+                    <h1 class="text-white">Simplify your inventory management, client communications, and more.
+                    </h1>
+                    <h2 class="p-t-1 text-white">Kabooodle hasdfasd fasdf asdf asdf asdfsa elps afjdsf asfajsfdsda sdadsaf asdf asfja f.</h2>
+                    <div class="m-t-3">
+                        <a class="btn btn-lg inverse white">Join Free Now</a>
+                    </div>
+
+                </div>
+
             </div>
         </div>
 
     </div>
 
+    <div style="background-color: #f9fafa; border-bottom-style: solid; border-width: 1px; border-color: #dedede;">
+        <div class="p-a">
+            asdf
+        </div>
+    </div>
 
+    <div style="height: 600px; padding-top: 60px" class="white" >
+        <div class="p-a">
+            <div class="row">
+                <div class="col-sm-12">
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
