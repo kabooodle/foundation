9afd66db22e2aa3e1a11356c798787065c31c021
Jake Toolson <jaketoolson@gmail.com>
2017-01-11 19:24:47 -0800

    .27

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -20,7 +20,7 @@ class KabooodleApplication extends Application
      * @var string
      */
     const APP_VERSION = '0.8.0';
-    const RELEASE_VERSION = '0.8.26';
+    const RELEASE_VERSION = '0.8.27';
 
     /**
      * @return void
