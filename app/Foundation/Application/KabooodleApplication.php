769d867338f7cb4d93790dee778efbf4c774277d
Jake Toolson <jaketoolson@gmail.com>
2017-01-21 10:44:35 -0800

    triggering another build to test npm cache

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -20,7 +20,7 @@ class KabooodleApplication extends Application
      * @var string
      */
     const APP_VERSION = '0.8.0';
-    const RELEASE_VERSION = '0.8.36';
+    const RELEASE_VERSION = '0.8.37';
 
     /**
      * @return void
