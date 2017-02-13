4c9ceac48aaffd4a86811c06449ae600d8925ebd
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-02-12 22:05:57 -0800

    fix inventory

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,8 +19,8 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.6';
-    const RELEASE_VERSION = '0.9.6';
+    const APP_VERSION = '0.9.7';
+    const RELEASE_VERSION = '0.9.7';
 
     /**
      * @return void
