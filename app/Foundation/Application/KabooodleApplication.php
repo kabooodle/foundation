fb6ad87398bf2e618e78a71b3c80a50e9ff56f7b
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-01 14:42:53 -0800

    roll cache bitch

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,8 +19,8 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.23';
-    const RELEASE_VERSION = '0.9.23';
+    const APP_VERSION = '0.9.24';
+    const RELEASE_VERSION = '0.9.24';
 
     /**
      * @return void
