5ba5548ed6c559a50b5a71230404574357026745
Jake Toolson <jaketoolson@gmail.com>
2017-03-04 00:42:37 -0800

    bumpy

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,8 +19,8 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.26';
-    const RELEASE_VERSION = '0.9.26';
+    const APP_VERSION = '0.9.27';
+    const RELEASE_VERSION = '0.9.27';
 
     /**
      * @return void
