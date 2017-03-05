04ec49cba37139d371c936bfe978705e954f4dae
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-04 19:38:24 -0800

    - home text fix - exception handler fix

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,8 +19,8 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.30';
-    const RELEASE_VERSION = '0.9.30';
+    const APP_VERSION = '0.9.31';
+    const RELEASE_VERSION = '0.9.31';
 
     /**
      * @return void
