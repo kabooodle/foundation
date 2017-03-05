8072e976e19b7ab674cdba64818a2559db5f6fb1
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-04 17:22:23 -0800

    fix to / route upgrade vue to 2.2

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.28';
+    const APP_VERSION = '0.9.29';
     const RELEASE_VERSION = '0.9.28';
 
     /**
