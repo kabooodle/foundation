204dedbea8a3340f3c4e902b9db55c5ef621ae8b
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-04 18:23:10 -0800

    - remove erroneous console.log

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,8 +19,8 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.29';
-    const RELEASE_VERSION = '0.9.29';
+    const APP_VERSION = '0.9.30';
+    const RELEASE_VERSION = '0.9.30';
 
     /**
      * @return void
