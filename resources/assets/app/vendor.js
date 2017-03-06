bfa99489fc1dd33f633a0851e3e5fd42b72536bb
Jake Toolson <jaketoolson@gmail.com>
2017-03-05 21:29:20 -0800

    trtying to trigger cache

--- resources/assets/app/vendor.js
+++ resources/assets/app/vendor.js
@@ -5,14 +5,14 @@ global.Tether = require('tether');
 require('bootstrap');
 global._ = require('lodash');
 global.moment = require('moment');
-require('moment-timezone');
+
 global.slider = require('bootstrap-slider');
+
 // Use localized version of Noty because I've heavily modified it :)
 require('../../../resources/assets/vendor/jquery/noty/packaged/jquery.noty.packaged.js');
 
 global.Clipboard = require('clipboard');
 require('ekko-lightbox');
-// require('selectize'); // Removed
 require('select2');
 require('emojione');
 require('emojionearea');
@@ -21,12 +21,12 @@ global.datetimepicker = require('eonasdan-bootstrap-datetimepicker');
 require('../vendor/tablesaw/tablesaw');
 require('perfect-scrollbar/jquery')($);
 require('bootstrap-select');
+require('moment-timezone');
 //
 // // Use the localized version because the NPM version is being fuckity.
 global.multiselect = require('../vendor/bootstrap-multiselect/dist/js/bootstrap-multiselect');
 
 import VueTimeago from 'vue-timeago';
-
 
 Vue.use(VueResource);
 Vue.use(VueTimeago, {
