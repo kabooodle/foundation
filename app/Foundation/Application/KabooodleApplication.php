871de796c400927871ef675c19df002ebd61015d
Jake Toolson <jaketoolson@gmail.com>
2017-03-05 22:02:24 -0800

    sigh

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,8 +19,8 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.40';
-    const RELEASE_VERSION = '0.9.40';
+    const APP_VERSION = '0.9.41';
+    const RELEASE_VERSION = '0.9.41';
 
     /**
      * @return void
--- resources/assets/app/vendor.js
+++ resources/assets/app/vendor.js
@@ -4,12 +4,10 @@ global.VueResource = require('vue-resource');
 global.Tether = require('tether');
 require('bootstrap');
 global._ = require('lodash');
-// global.moment = require('moment');
 global.moment = require('moment-timezone');
 
 global.slider = require('bootstrap-slider');
 
-// Use localized version of Noty because I've heavily modified it :)
 require('../../../resources/assets/vendor/jquery/noty/packaged/jquery.noty.packaged.js');
 
 global.Clipboard = require('clipboard');
@@ -22,15 +20,14 @@ global.datetimepicker = require('eonasdan-bootstrap-datetimepicker');
 require('../vendor/tablesaw/tablesaw');
 require('perfect-scrollbar/jquery')($);
 require('bootstrap-select');
-//
-// // Use the localized version because the NPM version is being fuckity.
+
 global.multiselect = require('../vendor/bootstrap-multiselect/dist/js/bootstrap-multiselect');
 
 import VueTimeago from 'vue-timeago';
 
 Vue.use(VueResource);
 Vue.use(VueTimeago, {
-    name: 'timeago', // component name, `timeago` by default
+    name: 'timeago',
     locale: 'en-US',
     locales: {
         'en-US': require('vue-timeago/locales/en-US.json')
