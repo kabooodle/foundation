1f8a04013863b89f5a084cc2c50e520b5b48d56a
Jonathan Pickett <jonathanpickett@me.com>
2017-03-15 11:57:42 -0700

    Fix user registration timezone data

--- resources/assets/app/components/register/Register.vue
+++ resources/assets/app/components/register/Register.vue
@@ -52,6 +52,8 @@
             <label>Referred By User <small class="">(username)</small></label>
         </div>
 
+        <input type="hidden" v-model="timezone" name="timezone">
+
         <p class="text-sm">By clicking on "Create Account" below, you are agreeing to the <a href="/legal/terms-service" class="text-primary">Terms of Service</a> and the <a href="/legal/privacy" class="text-primary">Privacy Policy</a>.</p>
 
         <button type="submit" class="btn primary btn-block p-x-md">Create Account</button>
@@ -84,6 +86,7 @@
                 username: null,
                 email: null,
                 password: null,
+                timezone: jstz.determine().name(),
             }
         },
         created(){
