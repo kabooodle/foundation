f3d04ed205104adc7607c4a3bfd48ec71b13240b
Jake Toolson <jaketoolson@gmail.com>
2017-03-16 14:57:23 -0700

    bump

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -20,7 +20,7 @@ class KabooodleApplication extends Application
      * @var string
      */
     const APP_VERSION = '0.9.80';
-    const RELEASE_VERSION = '0.9.79';
+    const RELEASE_VERSION = '0.9.80';
 
     /**
      * @return void
