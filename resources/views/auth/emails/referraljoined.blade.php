52276125176e8a8554a680c00a1b2d13fae038b1
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-17 11:21:23 -0700

    - referral joined email

--- resources/views/auth/emails/referraljoined.blade.php
+++ resources/views/auth/emails/referraljoined.blade.php
@@ -1,4 +1,6 @@
-{{ $referee->full_name }}, thank you for referring {!! $user->full_name !!} ({{ $user->username }}) to {{env('APP_NAME')}} today! We thank you for the referral.
+<p>{{ $referee->full_name }}, thank you for referring {!! $user->full_name !!} ({{ $user->username }}) to {{env('APP_NAME')}} today!</p>
+<p>To view all your referrals, credits, and prizes, visit your referrals page here: <a href="{{ route('referrals.index') }}">{{ route('referrals.index') }}</a></p>
+<p>Thank you!</p>
 
 --
 the {{ env('APP_NAME') }} Team
