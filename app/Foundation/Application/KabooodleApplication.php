9eb976535775f0858669c317b03a9a6e5eeb51b4
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-16 19:06:01 -0700

    bump version

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.80';
+    const APP_VERSION = '0.9.81';
     const RELEASE_VERSION = '0.9.81';
 
     /**
