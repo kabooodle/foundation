a21416bfad08f0cd4e7ae825e2bf9a6b565b89e6
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-20 01:53:45 -0700

    trying to debug debugging listable page

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.93';
+    const APP_VERSION = '0.9.94';
     const RELEASE_VERSION = '0.9.89';
 
     /**
