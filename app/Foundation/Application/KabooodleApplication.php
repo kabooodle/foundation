b375ae9e91352a99de810c987b8fcedaf13986a9
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-19 23:24:28 -0700

    possible fix? don't fucking know.

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.89';
+    const APP_VERSION = '0.9.90';
     const RELEASE_VERSION = '0.9.89';
 
     /**
