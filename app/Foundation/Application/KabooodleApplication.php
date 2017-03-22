03698810ddc230154d0e5a9b45ad8a8422d3a6eb
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-21 22:03:21 -0700

    merged master

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -20,7 +20,7 @@ class KabooodleApplication extends Application
      * @var string
      */
     const APP_VERSION = '0.9.99';
-    const RELEASE_VERSION = '0.9.89';
+    const RELEASE_VERSION = '0.9.99';
 
     /**
      * @return void
