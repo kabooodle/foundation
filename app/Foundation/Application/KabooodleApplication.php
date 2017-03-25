93661406eb3f97be58c94abaf8db70998410d9cd
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-24 22:13:31 -0700

    so I guess 8 is the magic number.

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.111';
+    const APP_VERSION = '0.9.112';
     const RELEASE_VERSION = '0.9.103';
 
     /**
