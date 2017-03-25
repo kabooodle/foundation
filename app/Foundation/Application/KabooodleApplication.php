6e246bab8ea39d0d7e6c2c68e6fe2ab4727f0d9a
Jake Toolson <jaketoolson@gmail.com>
2017-03-25 08:34:57 -0700

    adjusted the fuzzy search

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.113';
+    const APP_VERSION = '0.9.114';
     const RELEASE_VERSION = '0.9.103';
 
     /**
