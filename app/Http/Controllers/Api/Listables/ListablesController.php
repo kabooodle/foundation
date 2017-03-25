a0a5305022c9a7471335b8e39314e50e50f703aa
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-24 21:28:13 -0700

    change chunk to 8

--- app/Http/Controllers/Api/Listables/ListablesController.php
+++ app/Http/Controllers/Api/Listables/ListablesController.php
@@ -40,7 +40,7 @@ class ListablesController extends AbstractApiController
             $grouped = $inventory->groupBy('inventory_type_styles_id');
 
             // Group them together in groups of 6
-            $chunks = $grouped->chunk(3);
+            $chunks = $grouped->chunk(5);
 
             // Get the current page
             $currentPage = $request->has('page') ? $request->get('page') : 1;
