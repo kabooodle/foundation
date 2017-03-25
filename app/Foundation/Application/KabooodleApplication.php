770bba7d104aad3c1ed1f942a796aeb92561b0de
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-03-24 21:51:39 -0700

    chunk of 12??

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.109';
+    const APP_VERSION = '0.9.110';
     const RELEASE_VERSION = '0.9.103';
 
     /**
