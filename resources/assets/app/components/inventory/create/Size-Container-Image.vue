c4a8b21e0b6da6f4a75014fc35375181c992adb2
Jonathan Pickett <jonathanpickett@me.com>
2017-03-31 11:54:02 -0700

    Prevent reset of quantity when adding inventory images

---
+++ resources/assets/app/components/inventory/create/Size-Container-Image.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="col-sm-4 thumbnail-container m-b-2">
+        <div class="box no-shadow m-b-0 p-t-1 p-b-1 r b-1 b b-a" >
+            <div class="item" >
+                <div class="item-overlay active p-l p-r " style="z-index: 999;">
+                    <a type="button" style=""
+                       @click="deleteImage(image, $event)"
+                       :data-id="image.id"
+                       class="pull-right text-danger"><i class="fa fa-times fa-fw"></i></a>
+                    <span class="pull-left label dark-white text-color">{{ position }}</span>
+                </div>
+
+                <div class="thumbnail">
+                    <img :src="image.location"
+                         data-width="100%" class="w-full"
+                         :data-remote="image.location"/>
+                </div>
+            </div>
+            <input type="hidden"
+                   :name="'sizings['+sizingId +'][images]['+image.key+'][data]'" :value="image.json" />
+            <div class="box-body m-t-0 p-b-0 p-t-0">
+                <div class="image_item_component">
+                    <label class="text-muted text-sm m-b-0 p-b-0">Quantity:</label>
+                    <input type="text"
+                       v-model="quantity"
+                       :name="'sizings['+sizingId+'][images]['+image.key+'][qty]'"
+                       :id="quantityBtnId"
+                       class="text-center image_qty_btn"  />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+    
+</style>
+<script>
+    export default {
+        props: {
+            sizingId: {
+                required: true,
+                type: String,
+            },
+            quantity: {
+                required: true,
+            },
+            position: {
+                required: true,
+                type: Number,
+            },
+            image: {
+                required: true,
+                type: Object,
+            }
+        },
+        data () {
+            return {
+                quantityBtnId: 'image_qty_btn_'+this.image.hash,
+            }
+        },
+        created() {
+
+        },
+    }
+</script>
--- resources/assets/app/components/inventory/create/Size-Container.vue
+++ resources/assets/app/components/inventory/create/Size-Container.vue
@@ -7,83 +7,60 @@
                     <div class="btn-group-prpl" data-toggle="buttons">
                         <label class="form-control-label btn white" :key="size.id" v-for="size in sizings" style="margin-right: 3px;">
                             <input
-                                    required
-                                    aria-required="true"
-                                    validation="required"
-                                    type="radio"
-                                    :name="'sizings['+id +'][size_id]'" id="option1"
-                                    autocomplete="off"
-                                    :value="size.id"> {{ size.name }}
+                                required
+                                aria-required="true"
+                                validation="required"
+                                type="radio"
+                                :name="'sizings['+id +'][size_id]'" id="option1"
+                                autocomplete="off"
+                                :value="size.id"> {{ size.name }}
                         </label>
                     </div>
                 </div>
             </div>
             <div class="row row-horizon clearfix" style="position: relative; overflow: auto; ">
-                <div class="col-sm-4 thumbnail-container m-b-2"  v-for="(image, $index) in images" :key="id+'_'+image.id">
-                    <div class="box no-shadow m-b-0 p-t-1 p-b-1 r b-1 b b-a" >
-                        <div class="item" >
-                            <div class="item-overlay active p-l p-r " style="z-index: 999;">
-                                <a type="button" style=""
-                                   @click="deleteImage(image, $event)"
-                                   :data-id="image.id"
-                                   class="pull-right text-danger"><i class="fa fa-times fa-fw"></i></a>
-                                <span class="pull-left label dark-white text-color">{{ images.length - $index }}</span>
-                            </div>
-
-                            <div class="thumbnail">
-                                <img :src="image.location"
-                                    data-width="100%" class="w-full"
-                                     :data-remote="image.location"/>
-                            </div>
-                        </div>
-                        <input type="hidden"
-                               :name="'sizings['+id +'][images]['+image.key+'][data]'" :value="image.json" />
-                        <div class="box-body m-t-0 p-b-0 p-t-0">
-                            <div class="image_item_component">
-                                <label class="text-muted text-sm m-b-0 p-b-0">Quantity:</label>
-                                <input type="text"
-                                       value="1"
-                                       :name="'sizings['+id+'][images]['+image.key+'][qty]'"
-                                       class="text-center image_qty_btn"  />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <size-container-image v-for="(image, index) in images" :key="image.id"
+                    :sizing-id="id"
+                    :position="images.length - index"
+                    :image="image"
+                    :quantity="image.quantity"
+                >
+                </size-container-image>
             </div>
 
         </div>
         <div class="box-footer">
             <div class="form-group categories_wrapper">
                 <multiselect
-                        v-if="images.length > 0 && display.categories"
-                        v-model="category"
-                        tag-placeholder="Add this as a new category"
-                        placeholder="Add categories"
-                        label="name"
-                        track-by="name"
-                        :options="categories"
-                        :multiple="true"
-                        :taggable="true"
-                        @remove="removeTag"
-                        @tag="addTag">
+                    v-if="images.length > 0 && display.categories"
+                    v-model="category"
+                    tag-placeholder="Add this as a new category"
+                    placeholder="Add categories"
+                    label="name"
+                    track-by="name"
+                    :options="categories"
+                    :multiple="true"
+                    :taggable="true"
+                    @remove="removeTag"
+                    @tag="addTag">
                 </multiselect>
                 <template v-for="category in categories">
                     <input type="hidden"
-                           :value="category.name"
-                           :name="'sizings['+id+'][categories][]'"
-                           class="selectized">
+                       :value="category.name"
+                       :name="'sizings['+id+'][categories][]'"
+                       class="selectized">
                 </template>
             </div>
             <div class="clearfix">
                 <div class="row">
                     <div class="col-sm-offset-3 col-sm-7">
-                                        <span class="pull-left add-images-btn">
-                                            <image-attach
-                                                :ukey="id"
-                                                :s3_bucket="s3_bucket"
-                                                :s3_acl="s3_acl"
-                                                :s3_key_url="s3_key_url" ></image-attach>
-                                        </span>
+                        <span class="pull-left add-images-btn">
+                            <image-attach
+                                :ukey="id"
+                                :s3_bucket="s3_bucket"
+                                :s3_acl="s3_acl"
+                                :s3_key_url="s3_key_url" ></image-attach>
+                        </span>
 
                         <button type="button" class="pull-left btn add-categories-btn white btn-sm "
                             :disabled="images.length == 0"
@@ -96,6 +73,7 @@
 </template>
 <style src="./../../multiselect/vue-multiselect.min.css"></style>
 <script>
+    import SizeContainerImage from './Size-Container-Image.vue';
     import FileUpload from  '../../FileUpload.vue';
     import Multiselect from 'vue-multiselect';
 
@@ -132,17 +110,32 @@
             }
         },
         mounted(){
+            String.prototype.hashCode = function() {
+                var hash = 0, i, chr;
+                if (this.length === 0) return hash;
+                for (i = 0; i < this.length; i++) {
+                    chr   = this.charCodeAt(i);
+                    hash  = ((hash << 5) - hash) + chr;
+                    hash |= 0; // Convert to 32bit integer
+                }
+                return hash;
+            };
             this.$nextTick(()=>{
                 $('.row-horizon').perfectScrollbar();
             });
 
             $Bus.$on('image:uploaded:'+this.id, (el, responseData)=>{
+                responseData.quantity = 1;
+                responseData.hash = responseData.id.hashCode();
                 this.images.unshift(responseData);
 
                 this.$nextTick(()=>{
                     $('.row-horizon').perfectScrollbar('update');
-                    $('#size_'+this.id).find("input.image_qty_btn").TouchSpin({
+                    $("input#image_qty_btn_"+responseData.hash).TouchSpin({
                         min: 1
+                    }).change((data)=>{
+                        var index = this.images.indexOf(responseData);
+                        this.images[index].quantity = data.target.value;
                     });
                 });
             });
@@ -175,6 +168,7 @@
         components:{
             'multiselect' : Multiselect,
             'image-attach' : FileUpload,
+            'size-container-image' : SizeContainerImage,
         }
     }
 </script>
