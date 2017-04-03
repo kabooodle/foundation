b2f8ada6787e1449b2349dea8169a5937a6aad85
Jake Toolson <jake.toolson@fire-engine-red.com>
2017-04-03 09:02:13 -0700

    apparently i confused the home.js file I previously removed :o

--- app/Foundation/Application/KabooodleApplication.php
+++ app/Foundation/Application/KabooodleApplication.php
@@ -19,7 +19,7 @@ class KabooodleApplication extends Application
     /**
      * @var string
      */
-    const APP_VERSION = '0.9.125';
+    const APP_VERSION = '0.9.127';
     const RELEASE_VERSION = '0.9.125';
 
     /**
