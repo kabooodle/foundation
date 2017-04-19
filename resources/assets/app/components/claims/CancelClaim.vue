105482481e29c51cd40a4c684d897188e2617dfb
Jonathan Pickett <jonathanpickett@me.com>
2017-04-18 22:14:14 -0700

    Custom cancel claim modal with ability to add cancel message

--- resources/assets/app/components/claims/CancelClaim.vue
+++ resources/assets/app/components/claims/CancelClaim.vue
@@ -1,10 +1,25 @@
 <template>
-    <a @click="cancel" class="btn btn-xs danger" :class="btnDisabled ? 'disabled' : null">Cancel<spinny v-if="cancelling"></spinny></a>
+    <span>
+        <a @click="showCancelModal" class="btn btn-xs danger" :class="btnDisabled ? 'disabled' : null">Cancel<spinny v-if="cancelling"></spinny></a>
+        <modal :modal_id="modalId" modal_class="cancel-modal" :display_header="false" :display_footer="false">
+            <div slot="modal_body">
+                <div class="m-a-2 center text-center block-center center-block">
+                    <h5 class="m-t-3 m-b-3">Are you sure you want to cancel this claim? This cannot be undone.</h5>
+                    <div class="form-group">
+                        <textarea id="comment_new_text" v-model="cancelMessage" class="form-control" rows="3" placeholder="I am cancelling this claim because..." style="resize: none;"></textarea>
+                    </div>
+                    <button type="button" class="m-l-1 btn btn-lg white" @click.prevent="hideCancelModal">Don't cancel claim</button>
+                    <button type="button" class="btn btn-lg danger" :class="submittingCancel ? 'disabled' : null" @click.prevent="cancel">Yes, cancel claim<spinny v-if="submittingCancel"></spinny></button>
+                </div>
+            </div>
+        </modal>
+    </span>
 </template>
 <style>
     
 </style>
 <script>
+    import Modal from '../Modal.vue';
     import Spinny from '../Spinner.vue';
     export default {
         props: {
@@ -19,21 +34,38 @@
         },
         data () {
             return {
-                cancelling: false
+                cancelling: false,
+                submittingCancel: false,
+                modalId: 'cancel-modal-'+this.claim.id,
+                cancelMessage: null,
             }
         },
         computed: {
             btnDisabled() {
                 return !this.claim.cancelable || this.claim.canceled_at || this.cancelling;
+            },
+            cancelData() {
+                return {
+                    message: this.cancelMessage
+                }
             }
         },
         methods: {
-            cancel() {
+            showCancelModal() {
                 this.cancelling = true;
+                $('#'+this.modalId).modal('show');
+            },
+            hideCancelModal() {
+                console.log('hiding');
+                this.cancelling = false;
+                $(document).find('.cancel-modal').modal('hide');
+            },
+            cancel() {
+                this.submittingCancel = true;
                 var self = this;
-                confirmModal(function () {
-                    self.$http.post(self.claimEndpoint + '/cancel')
+                self.$http.post(self.claimEndpoint + '/cancel', self.cancelData)
                     .then(function (response) {
+                        self.hideCancelModal();
                         $.noty.closeAll();
                         self.claim.canceled_at = response.body.data.claim.canceled_at;
                         self.$emit('canceled');
@@ -48,15 +80,13 @@
                             'type': 'error'
                         });
                     }).finally(function () {
+                        self.submittingCancel = false;
                         self.cancelling = false;
                     });
-                }, function () {
-                    $.noty.close();
-                    self.cancelling = false;
-                }, {text: 'Are you sure you want to cancel this claim? This can not be undone.'});
             }
         },
         components: {
+            Modal,
             Spinny
         }
     }
